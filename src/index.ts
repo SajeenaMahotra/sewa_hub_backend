d1b48e5d0f1da21c5defb4c7e3578a5b14e2b491
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2026-01-04 14:25:41 +0545

    update server startup

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import express, {Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
+import authRoutes from "./routes/auth.route";
+
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({ success: "true", message: "Welcome to the API" });
+});
 
 async function startServer() {
     await connectDatabase();
