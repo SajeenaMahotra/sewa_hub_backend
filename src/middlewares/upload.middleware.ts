a11fa7f60c846e0ed0288470fab2479d2d426e72
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2026-01-27 21:40:23 +0545

    add authorization and admin middlewares

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,39 +1,37 @@
 import multer from "multer";
 import uuid from "uuid";
-import path from "path";
-import fs from "fs";
 import { Request } from "express";
+import path from "path";
 import { HttpError } from "../errors/http-error";
-
-// Ensure the uploads directory exists
-// __dirname - current directory of this file
-const uploadDir = path.join(__dirname, '../../uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir); // set upload directory
+        const uploadPath = path.join(__dirname, "../../uploads/");
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, path.join(__dirname, "../../uploads/"));
     },
-    filename: function (req: Request, file, cb) {
-        const ext = path.extname(file.originalname); // get file extension
-        const filename = `${uuid.v4()}${ext}`;
-        cb(null, filename); // set unique file name
+    filename: function (req, file, cb) {
+        const fileSuffix = uuid.v4();
+        cb(null, fileSuffix + '-' + file.originalname);
     }
 });
+
 const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    // Accept images only
-    if (!file.mimetype.startsWith('image/')) {
-        return cb(new HttpError(400, 'Only image files are allowed!'));
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new HttpError(400, 'Invalid file type, only JPEG and PNG is allowed!'));
     }
-    cb(null, true);
-}
+};
 
 const upload = multer({
     storage: storage,
-    fileFilter: fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024 } // 5 MB limit
+    limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
+    fileFilter: fileFilter
 });
 
 export const uploads = {
