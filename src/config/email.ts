3bf8d2419d4ab59ff30298deecd748f36022dc1f
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2026-02-12 06:53:43 +0545

    debug email config

--- src/config/email.ts
+++ src/config/email.ts
@@ -2,6 +2,13 @@ import nodemailer from 'nodemailer';
 const EMAIL_PASS=process.env.EMAIL_PASS as string;
 const EMAIL_USER=process.env.EMAIL_USER as string;
 
+// Add this check
+console.log('Email Config:', {
+    user: EMAIL_USER,
+    passLength: EMAIL_PASS?.length,
+    passPreview: EMAIL_PASS?.substring(0, 4) + '****'
+});
+
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
